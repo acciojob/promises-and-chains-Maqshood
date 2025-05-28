bf74dfca2b0b157a8392a3eaad77d2f64cbbf283
SK Maqshood <skmaqshood99@gmail.com>
2025-05-29 00:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form from actually submitting
+
+      const name = document.getElementById("name").value.trim();
+      const age = document.getElementById("age").value.trim();
+
+      if (name === "" || age === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+	const ageNumber=parseInt(age);
+	const checkEligibilty= new promise((resolve,reject)=>{
+setTimeout(()=>{
+	if(ageNumber>18){
+	resolve(`Welcome,${name}. You can vote.`);
+		}else{
+		reject(`Oh sorry ${name}. You aren't old enough.`);
+		}
+},4000);
+});
+ 
+ checkEligibility
+        .then((message) => {
+          alert(message);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    });
+
+
+	
